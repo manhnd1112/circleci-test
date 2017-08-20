6418ec99e56fcee82c9902dbe5e294dd369d7977
minhnc <minhnc.4a@gmail.com>
2017-08-20 17:05:23 +0700

    add code test

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by minhnc on 8/20/17.
+ */
+public class App {
+    public static void main(String[] args) {
+         System.out.println("Test CricleCI");
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by minhnc on 8/20/17.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertEquals(1,1);
+    }
+}
